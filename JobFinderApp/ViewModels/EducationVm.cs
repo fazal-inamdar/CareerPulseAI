e1cd65aa1e2d5e9ebb2d80924a7cd04f3721ba8a
Sabahfakir <112008045+Sabahfakir@users.noreply.github.com>
2026-09-05 17:07:38 +0530

    Delete JobFinderApp directory

--- JobFinderApp/ViewModels/EducationVm.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace JobFinderApp.ViewModels
-{
-    public class EducationVm
-    {
-        public string Degree { get; set; }
-        public string Institution { get; set; }
-        public string Year { get; set; }
-    }
-}
